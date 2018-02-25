869c56371d90bd30fe1b9b25fcc05c5d53090a61
Tom Cole <colecode.ph@gmail.com>
2018-02-25 20:27:47 +0800

    Add classname and textContent on li click event

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-names = [ 'spot', 'cranky', 'kittens', 'hans', 'stooges' ]
+var animals = {
+    names: [ 'spot', 'cranky', 'kittens', 'hans', 'stooges' ],
+    clicks: [ 0, 0, 0, 0, 0 ]
+}
 
 animalList = document.getElementById('animalList'); // this is the <ul> (GLOBAL)
 
 // loop through the names and create <li>s and <a>s with animal name ids
-names.forEach( function(animalName) {
+animals.names.forEach( function(animalName) {
     var li = document.createElement('li');
     var a = document.createElement('a');
     a.textContent = animalName;
@@ -18,22 +21,40 @@ animalList.addEventListener("click", function(event) {
     // assign the clicked target <a> element value to elementClicked
     var elementClicked = event.target;
     // loop through the array of names
-    names.forEach( function(animalName) {
+    animals.names.forEach( function(animalName) {
         // if the name matches the <a> element's id we change the image src
         if (elementClicked.id === animalName) {
             // images are named accordingly, ie spot's image is spot.jpg
-            document.getElementById("animalImage").src="images/" + animalName + ".jpg";
+            // also change the class name to the animalName value
+            document.getElementById("animalImage").src=("images/" + animalName + ".jpg");
+            document.getElementById("animalImage").className = animalName;
+            document.getElementById("animalIncrementHeader").textContent =
+                "You Have Clicked " + animalName + "'s image 0 times."
         }
     });
 });
 
+// animalImage = document.getElementById('animalImage'); // this is the <img> (GLOBAL)
+
+// // add click event listener to the <img>
+// animalImage.addEventListener("click", function(event) {
+//      // loop through the array of names
+//      names.forEach( function(animalName) {
+//          // if the image class matches the animal name
+//          if animalImage.classList.contains(animalName) {
+//             // do stuff
+//          }
+//      });
+//  });
 
 
-clicks = function (name, count) {
+
+
+// clicks = function (name, count) {
         // find out how many clicks an animal has
         // event listener to register a new click
         // increment the value of count to the new value and it somewhere?
-    };
+  //  };
 // var catClicks = 0
 // catImage.addEventListener('click', function(){
 //     // increment clicks variable
