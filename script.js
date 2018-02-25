4152113dbdd6e3ba8723bcd6f55ef58eedcf416f
Tom Cole <colecode.ph@gmail.com>
2018-02-25 23:25:23 +0800

    Comment cleanup

--- script.js
+++ script.js
@@ -57,24 +57,3 @@ animalImage.addEventListener("click", function(event) {
     document.getElementById("animalIncrementHeader").textContent =
         "You have clicked " + animalName + "'s image " + newClickValue + " times.";
  });
-
-
-
-
-// clicks = function (name, count) {
-        // find out how many clicks an animal has
-        // event listener to register a new click
-        // increment the value of count to the new value and it somewhere?
-  //  };
-// var catClicks = 0
-// catImage.addEventListener('click', function(){
-//     // increment clicks variable
-//     catClicks += 1;
-//     // retrieve the h2 element with id incrementHeader
-//     var catH1 = document.getElementById('catName');
-//     var incrementHeader = document.getElementById('catIncrementHeader');
-//     // console.log(incrementHeader);
-//     // modify the text so that the value increments
-//     catH1.textContent = ("Cat Clicker for " + catName);
-//     incrementHeader.textContent = ("You have clicked on the Cat " + catClicks + " Times.");
-// }, false);
